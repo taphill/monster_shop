02cfaa1ed8a1fb1360101b8e3e2d640c0a1e7e81
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-27 15:22:16 -0500

    Add flash messaged for new user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    if params[:password] == params[:password_confirmation]
-      new_user = User.create(user_params)
+    new_user = User.new(user_params)
+
+    if params[:password] == params[:password_confirmation] && new_user.valid?
+      new_user.save
       flash[:success] = 'You are now registered and logged in!'
       session[:user_id] = new_user.id
       redirect_to "/profile"
+    elsif params[:password] != params[:password_confirmation]
+      flash[:error] = 'Your passwords do not match' 
+      redirect_to "/register"
+    else
+      flash[:error] = 'Missing required fields' 
+      redirect_to "/register"
     end
   end
 
