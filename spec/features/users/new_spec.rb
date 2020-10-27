7172a21198da1fa828e69170ee6a5903e82c6750
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-27 15:21:53 -0500

    Add test for new user required fields

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -31,5 +31,22 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_content("You are now registered and logged in!")
       expect(page).to have_content("Hello, #{user.name}")
     end
+
+    it 'returns to the registration page with flash message when form is not filled out' do
+      visit '/register'
+
+      fill_in :street_address, with: "1234 America Lane"
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80017"
+      fill_in :email, with: "jun.lee@gmail.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+
+      click_button "Submit"
+
+      expect(current_path).to eq('/register')
+      expect(page).to have_content('Missing required fields')
+    end
   end
 end
