0c17ec6448554899e181d7ec0580d6b3847900bb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-28 15:58:08 -0600

    Update create controller with flash message when creds are incorrect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,17 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(login_params)
-    session[:user_id] = @user.id
-    flash[:notice] = "Welcome, #{@user.name}"
+    session[:user_id] = @user.id if @user.authenticate(params[:password])
+    flash[:notice] = "Welcome, #{@user.name}" if current_user
     if current_merchant?
       redirect_to '/merchant'
     elsif current_admin?
       redirect_to '/admin'
-    else
+    elsif current_user
       redirect_to '/profile'
+    else
+      flash[:error] = "Your login credentials were incorrect."
+      redirect_to login_path
     end
   end
 
