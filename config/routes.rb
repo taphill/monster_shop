89c03e1d0594b79c9ecad80997bbdbb905be4d62
Kate Tester <katemorris@gmail.com>
2020-10-28 10:38:51 -0600

    Adds home and login routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'welcome#index'
+  get "/login", to: "sessions#new"
+
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
