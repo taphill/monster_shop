c82365845abef4abbea5db1ca9f15eff4cbf3049
Kate Tester <katemorris@gmail.com>
2020-10-28 12:29:38 -0600

    Adds enum for roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password   
+  validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password
   validates :email, uniqueness: true, presence: true
+
+  enum role: ['default', 'merchant']
 
   def email_exists?
     User.exists?(email: self.email)
