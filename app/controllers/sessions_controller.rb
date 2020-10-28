491b553d32cabd394a447753171dbf33ea930da5
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-28 16:05:48 -0600

    Update create action in sessions controller with password authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(login_params)
-    session[:user_id] = @user.id if @user.authenticate(params[:password])
-    flash[:notice] = "Welcome, #{@user.name}" if current_user
-    if current_merchant?
-      redirect_to '/merchant'
-    elsif current_admin?
-      redirect_to '/admin'
-    elsif current_user
-      redirect_to '/profile'
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}" if current_user
+      return redirect_to '/merchant' if current_merchant?
+      return redirect_to '/admin' if current_admin?
+      return redirect_to '/profile' if current_user
     else
       flash[:error] = "Your login credentials were incorrect."
       redirect_to login_path
