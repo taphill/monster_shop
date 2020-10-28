7c876169200e5ee0ed3ca4510ad0c034d2209bf1
Kate Tester <katemorris@gmail.com>
2020-10-28 11:20:40 -0600

    Adds path to login a user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,16 @@ class SessionsController < ApplicationController
   def new
     @session = session[:user_login]
   end
+
+  def create
+    @user = User.find_by(login_params)
+    session[:user_id] = @user.id
+    redirect_to '/profile'
+  end
+
+  private
+
+  def login_params
+    params.permit(:email)
+  end
 end
