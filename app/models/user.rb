e36c6370f92f9facec219c2943ca48bccda2c1b0
Kate Tester <katemorris@gmail.com>
2020-10-28 14:39:48 -0600

    Adds admin role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password
   validates :email, uniqueness: true, presence: true
 
-  enum role: ['default', 'merchant']
+  enum role: ['default', 'merchant', 'admin']
 end
