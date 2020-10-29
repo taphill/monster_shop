90225148180e7a9e39189ea854410d5029f9a727
Joshua Carey <jdcarey128@gmail.com>
2020-10-29 12:02:08 -0600

    Add password not presence expectation

--- spec/features/users/registered_user_profile_spec.rb
+++ spec/features/users/registered_user_profile_spec.rb
@@ -17,9 +17,10 @@ RSpec.describe "As a registered user", type: :feature do
         expect(page).to have_content(user.email)
         expect(page).to have_content(user.created_at.strftime("%m/%d/%Y"))
         expect(page).to have_link("Edit Profile Data")
+        expect(page).to_not have_content(user.password)
       end
 
     end
-    
+
   end
 end
