da4a613adcd82ec4a81f835e532879add0258cd3
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-29 20:04:44 -0600

    Update Order model with enum for status

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,11 @@
-class Order <ApplicationRecord
+class Order < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
 
   has_many :item_orders
   has_many :items, through: :item_orders
+  belongs_to :user
+
+  enum status:  %w(packaged pending shipped cancelled)
 
   def grandtotal
     item_orders.sum('price * quantity')
