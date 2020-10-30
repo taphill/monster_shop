247a14ac49a7e2e0e0406e53868980c18620766b
Joshua Carey <jdcarey128@gmail.com>
2020-10-29 20:37:48 -0600

    Update test to drop stub - which was messing up actual results

--- spec/features/users/registered_user_profile_spec.rb
+++ spec/features/users/registered_user_profile_spec.rb
@@ -19,7 +19,49 @@ RSpec.describe "As a registered user", type: :feature do
         expect(page).to have_link("Edit Profile Data")
         expect(page).to_not have_content(user.password)
       end
+    end
 
+    it "I can edit any/all of my profile data which is updated on my profile page" do
+      user = create(:user)
+
+      visit login_path
+
+      fill_in :email, with: user.email
+      fill_in :password, with: 'password'
+      click_button 'Login'
+
+      visit profile_path(user)
+
+      click_link("Edit Profile Data")
+
+      expect(current_path).to eq(profile_edit_path(user))
+
+      expect(find_field(:name).value).to eq(user.name)
+      expect(find_field(:street_address).value).to eq(user.street_address)
+      expect(find_field(:city).value).to eq(user.city)
+      expect(find_field(:state).value).to eq(user.state)
+      expect(find_field(:zip).value).to eq(user.zip)
+      expect(find_field(:email).value).to eq(user.email)
+      expect(page).to_not have_field(:password)
+      expect(page).to_not have_field(:password_confirmation)
+
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80209"
+
+      click_button "Update Profile"
+
+      expect(current_path).to eq(profile_path)
+
+      within ".user-profile" do
+        expect(page).to have_content(user.name)
+        expect(page).to have_content(user.street_address)
+        expect(page).to have_content("Denver")
+        expect(page).to have_content("CO")
+        expect(page).to have_content("80209")
+        expect(page).to have_content(user.email)
+        expect(page).to have_content(user.created_at.strftime("%m/%d/%Y"))
+      end
     end
 
   end
