17c3a336388a3f4950130be6dffa034a7dbbe21c
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-29 20:02:21 -0600

    Update users table with status column with default of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_002614) do
+ActiveRecord::Schema.define(version: 2020_10_30_020105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_30_002614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
