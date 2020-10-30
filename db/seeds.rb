31b29d4373909f6426d949677ca4e5166a83f564
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-30 09:02:15 -0500

    Add more seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,12 @@ dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', c
 
 #bike_shop items
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+bike = bike_shop.items.create(name: "Mountain Bike", description: "An amazing bike!", price: 2100, image: "https://www.bigandtallbike.com/assets/images/BIG%20six%20S%20tube%20gray%20ISO%202400px.png", inventory: 2)
+brakes = bike_shop.items.create(name: "Brakes", description: "Outstanding brakes!", price: 200, image: "https://content.backcountry.com/images/items/900/SHI/SHIU18H/BLA.jpg", inventory: 8)
+frame = bike_shop.items.create(name: "Frame", description: "You'll love thif frame!", price: 1000, image: "https://content.backcountry.com/images/items/900/SNZ/SNZK145/HIGBLU.jpg", inventory: 4)
 
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+frisbee = dog_shop.items.create(name: "Frisbee", description: "You can't beat the classics!", price: 12, image: "https://img.chewy.com/is/image/catalog/59178_MAIN._AC_SL1500_V1518037644_.jpg", inventory: 14)
+tennis_balls = dog_shop.items.create(name: "Tennis Balls", description: "Dogs love tennis balls!", price: 4, image: "https://www.tennisexpress.com/prodimages/45486-DEFAULT-l.jpg", inventory: 24)
