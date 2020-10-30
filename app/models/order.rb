9e1b42d4fe3482a859fe461359b43d27d7a19057
Kate Tester <katemorris@gmail.com>
2020-10-29 21:28:24 -0600

    Adds migration data from Shaundas work to relate users to orders.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order <ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
-
+  belongs_to :user
   has_many :item_orders
   has_many :items, through: :item_orders
+
+  enum status:  %w(pending packaged shipped cancelled)
 
   def grandtotal
     item_orders.sum('price * quantity')
