06b35d6eccdf58a8b75c474c584f4f384f149e1d
Joshua Carey <jdcarey128@gmail.com>
2020-10-29 19:26:19 -0600

    Modify password validation to allow for updates without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password
+  validates_presence_of :name, :street_address, :city, :state, :zip, :email
+  validates_presence_of :password, if: :password
   validates :email, uniqueness: true, presence: true
 
   enum role: ['default', 'merchant', 'admin']
