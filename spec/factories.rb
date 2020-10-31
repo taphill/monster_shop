89a9c94d71925936b0512e0cd9ec321aa28bc2fb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-30 20:30:10 -0600

    Remove merchant factory as to not conflict with another version already in PR status

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,12 +9,4 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
   end
-
-  factory :merchant do
-    name { Faker::Company.name }
-    address { Faker::Address.street_address }
-    city { Faker::Address.city }
-    state { Faker::Address.state_abbr }
-    zip { Faker::Address.zip }
-  end
 end
