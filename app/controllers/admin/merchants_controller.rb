83b0baf6a86a065c43ce64ceaf72f8b1483d3676
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-01 22:01:44 -0600

    Add functionality for user story 40

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -5,10 +5,23 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    merchant[:enabled] = false
-    merchant.save
 
-    flash[:success] = "#{merchant.name}'s account is now disabled"
-    redirect_to admin_merchants_path
+    if enable
+      merchant[:enabled] = true
+      merchant.save
+      flash[:success] = "#{merchant.name}'s account is now enabled"
+      redirect_to admin_merchants_path
+    else
+      merchant[:enabled] = false
+      merchant.save
+      flash[:success] = "#{merchant.name}'s account is now disabled"
+      redirect_to admin_merchants_path
+    end
+  end
+
+  private
+  
+  def enable
+    ActiveModel::Type::Boolean.new.cast(params[:enable])
   end
 end
