e617fb72c756c2f4e438cac20c0d5666e27ee8e5
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-03 12:30:30 -0700

    Add show action to merchant orders controller

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,6 +1,8 @@
-class Merchant::OrdersController < ApplicationController
+class Merchant::OrdersController < Merchant::BaseController
 
   def show
-  end 
+    @order = Order.find(params[:order_id])
+    @merchant_id = User.find(session[:user_id]).merchant_id
+  end
 
 end
