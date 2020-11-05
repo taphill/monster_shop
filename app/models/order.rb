82b71065c547360ee3a25087c1a98098bca15d1e
Joshua Carey <jdcarey128@gmail.com>
2020-11-05 14:20:51 -0700

    Refactor status_check and tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,28 +15,17 @@ class Order < ApplicationRecord
   end
 
   def status_check
-    require "pry"; binding.pry
     if self.item_orders.fulfilled.count == self.item_orders.count && self.status != "packaged"
       update(:status => "packaged")
     end
     self.status
   end
 
-  # def status_check
-  #   if all_fulfilled? && self.status != "packaged"
-  #     self.status = "packaged"
-  #     self.save
-  #     self.status
-  #   else
-  #     self.status
-  #   end
-  # end
-  #
-  # def all_fulfilled?
-  #   item_orders.all? do |io|
-  #     io.fulfill_status == "fulfilled"
-  #   end
-  # end
+  def all_fulfilled?
+    item_orders.all? do |io|
+      io.fulfill_status == "fulfilled"
+    end
+  end
 
   def merchant_items(merchant_id)
     items.where('merchant_id = ?', merchant_id)
