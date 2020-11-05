6973ebc54906e144b83f7854f68e9a08b1adf2a0
Kate Tester <katemorris@gmail.com>
2020-11-04 20:27:07 -0700

    Updates actions to be restful.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,12 @@
 class CartController < ApplicationController
-  def add_item
+  def create
     item = Item.find(params[:item_id])
     cart.add_item(item.id.to_s)
     flash[:success] = "#{item.name} was successfully added to your cart"
     redirect_to "/items"
   end
 
-  def change_quantity
+  def update
     item = Item.find(params[:item_id])
     if params[:type] == 'up'
       cart.add_item(item.id.to_s)
@@ -22,15 +22,19 @@ class CartController < ApplicationController
     @items = cart.items
   end
 
-  def empty
-    session.delete(:cart)
-    redirect_to '/cart'
+  def destroy
+    if params[:item_id]
+      session[:cart].delete(params[:item_id])
+      redirect_to '/cart'
+    else
+      session.delete(:cart)
+      redirect_to '/cart'
+    end
   end
 
-  def remove_item
-    session[:cart].delete(params[:item_id])
-    redirect_to '/cart'
-  end
+  # def remove_item
+  #
+  # end
 
 
 end
