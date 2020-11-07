7b95b1f8da50bfb243ab1d2bcb6fec93cd030357
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-07 17:53:22 -0600

    Add create and update actions to merchant/discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Merchant::DiscountsController < Merchant::BaseController
   def index
     @merchant = current_user.merchant if current_merchant?
@@ -13,14 +15,31 @@ class Merchant::DiscountsController < Merchant::BaseController
 
   def create
     merchant = Merchant.find(current_user.merchant_id)
-    discount = merchant.discounts.new(discount_params)
+    @discount = merchant.discounts.new(discount_params)
 
-    if discount.save
+    if @discount.save
       flash[:success] = 'New discount successfully created'
       redirect_to merchant_discounts_path
     else
-      flash[:error] = "We're sorry. Something went wrong"
+      flash_error
+      render :new
+    end
+  end
+
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    @discount.update(discount_params)
+
+    if @discount.save
+      flash[:success] = 'Discount successfully edited'
       redirect_to merchant_discounts_path
+    else
+      flash_error
+      render :edit
     end
   end
 
@@ -28,5 +47,9 @@ class Merchant::DiscountsController < Merchant::BaseController
 
   def discount_params
     params.require(:discount).permit(:percentage, :item_quantity)
+  end
+
+  def flash_error
+    flash[:error] = @discount.errors.full_messages.to_sentence
   end
 end
