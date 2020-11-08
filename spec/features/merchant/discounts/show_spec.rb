fac1922cd7b876a80218b852d3dede93e90be302
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-07 18:15:06 -0600

    Add test for mechant/discount destroy

--- spec/features/merchant/discounts/show_spec.rb
+++ spec/features/merchant/discounts/show_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "merchant/discounts/show", type: :feature do
   describe 'as a merchant' do
-    let!(:merchant) { create(:merchant, :with_discounts, discount_count: 3) }
+    let!(:merchant) { create(:merchant) }
+    let!(:discount) { create(:discount, merchant: merchant) }
     let!(:user) { create(:user, role: 1, merchant: merchant) }
 
     before do
@@ -11,12 +12,19 @@ RSpec.describe "merchant/discounts/show", type: :feature do
       fill_in :password, with: user.password
       click_button "Login"
 
-      visit merchant_discount_path(merchant.discounts[0])
+      visit merchant_discount_path(discount)
     end
 
     it 'can get to the edit discount page' do
       click_link 'Edit Discount'
-      expect(page).to have_current_path(edit_merchant_discount_path(merchant.discounts[0]))
+      expect(page).to have_current_path(edit_merchant_discount_path(discount))
+    end
+
+    it 'can delete a discount' do
+      click_button 'Delete Discount'
+      expect(page).to have_current_path(merchant_discounts_path)
+      expect(page).to_not have_content("#{discount.percentage}% discount on #{discount.item_quantity} or more items")
+      expect(page).to have_content('Discount has been deleted')
     end
   end
 end
