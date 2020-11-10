1a8e3759295163c8f23a5b5db278281f6d3e1e13
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-10 11:10:34 -0600

    Add test for Discount model instance methods

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -11,4 +11,49 @@ describe Discount, type: :model do
   describe 'relationship' do
     it { is_expected.to belong_to :merchant }
   end
+
+  describe 'instance methods' do
+    describe '#valid_item_quantity' do
+      context 'when a discount with a specific item_quanity does not already exist' do
+        it 'returns true' do
+          merchant = create(:merchant)
+          discount1 = build(:discount, item_quantity: 3) 
+
+          expect(discount1.valid_item_quantity?).to eq(true)
+        end
+      end
+
+      context 'else' do
+        it 'returns false' do
+          merchant = create(:merchant)
+          discount1 = create(:discount, item_quantity: 3) 
+          discount2 = build(:discount, item_quantity: 3) 
+      
+          expect(discount2.valid_item_quantity?).to eq(false)
+        end
+      end
+    end
+
+    describe '#logical_discount?' do
+      context 'when a disccount with a higher percentage and lower item threshold already exists' do
+        it 'returns false' do
+          merchant = create(:merchant)
+          discount1 = create(:discount, percentage: 10, item_quantity: 5) 
+          discount2 = build(:discount, percentage: 5, item_quantity: 15) 
+          
+          expect(discount2.logical_discount?).to eq(false)
+        end
+      end
+
+      context 'else' do
+        it 'returns true' do
+          merchant = create(:merchant)
+          discount1 = create(:discount, percentage: 10, item_quantity: 5) 
+          discount2 = build(:discount, percentage: 15, item_quantity: 15) 
+          
+          expect(discount2.logical_discount?).to eq(true)
+        end
+      end
+    end
+  end
 end
