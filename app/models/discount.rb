6854282f77b78cbf9768705db43186660c6bb398
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-09 19:40:47 -0600

    Add validation model methods for Discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,15 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   validates :percentage, presence: true
   validates :item_quantity, presence: true
+
+  def valid_item_quantity?
+    Discount.where(item_quantity: self.item_quantity).empty?
+  end
+
+  def logical_discount?
+    Discount.select(:percentage, :item_quantity)
+            .where('item_quantity < ?', self.item_quantity)
+            .where('percentage > ?', self.percentage)
+            .empty?
+  end
 end
