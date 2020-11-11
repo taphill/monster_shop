4424df2aa2436925a2d887da5ca143c35966a015
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-10 19:00:49 -0600

    Add valid_percentage method and refactor logical_discount method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -9,10 +9,27 @@ class Discount < ApplicationRecord
     Discount.where(item_quantity: self.item_quantity).empty?
   end
 
+  def valid_percentage?
+    Discount.where(percentage: self.percentage).empty?
+  end
+
   def logical_discount?
+    larger_discount_with_less_items? && smaller_discount_with_more_items?
+  end
+
+  private
+
+  def larger_discount_with_less_items?
     Discount.select(:percentage, :item_quantity)
             .where('item_quantity < ?', self.item_quantity)
             .where('percentage > ?', self.percentage)
+            .empty?
+  end
+
+  def smaller_discount_with_more_items?
+    Discount.select(:percentage, :item_quantity)
+            .where('item_quantity > ?', self.item_quantity)
+            .where('percentage < ?', self.percentage)
             .empty?
   end
 end
