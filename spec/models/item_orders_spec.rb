f36b3f89b31beba9965addc30021330b1ad9a507
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-11 00:15:19 -0600

    Add test for discount_applied method in ItemOrder model

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -43,4 +43,21 @@ describe ItemOrder, type: :model do
       end
     end
   end
+
+  describe 'instance methods' do
+    describe '#discount_applied' do
+      context 'when a discount was applied' do
+        it 'return the discount percentage applied' do
+          merchant = create(:merchant, :with_items, item_count: 3)
+          item_order1 = create(:item_order, item: merchant.items[0], discount: 0.05)
+          item_order2 = create(:item_order, item: merchant.items[1], discount: 0.10)
+          item_order3 = create(:item_order, item: merchant.items[2])
+
+          expect(item_order1.discount_applied).to eq(5)
+          expect(item_order2.discount_applied).to eq(10)
+          expect(item_order3.discount_applied).to be_nil
+        end
+      end
+    end
+  end
 end
